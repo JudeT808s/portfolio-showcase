b016d978a5f9b73f827eca8694066fac843f57bf
jude <jaytbas@gmail.com>
2025-04-10 15:53:07 +0100

    Fixed issue that nav burger not displaying properly

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,9 +17,17 @@ const Navbar = () => {
     <div className={(baseNavbarClass )}>
       <div className="navbar-start">
         <div className="dropdown lg:hidden">
-          <label tabIndex={0} className={(baseDropdownLabelClass)}>
-            <svg xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-          </label>
+        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="w-6 h-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" />
+  </svg>
+</label>
           <ul tabIndex={0} className={(baseDropdownClass)}>
             <li><Link to="/" className={(baseLinkClass)}>Home</Link></li>
             <li><Link to="/about" className={(baseLinkClass)}>About</Link></li>
