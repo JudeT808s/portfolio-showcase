21c8a8262e5714e7277b227e1a9ac512eab8af3a
jude <jaytbas@gmail.com>
2025-04-17 14:41:56 +0100

    added emissions badge

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-
 const Footer = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://unpkg.com/website-carbon-badges@1.1.3/b.min.js';
+    script.defer = true;
+    document.body.appendChild(script);
+  }, []);
 
   return (
-    <footer className={`footer p-10 mt-5 `}>
+    <footer className="footer p-10 mt-5">
       <nav>
         <header className="footer-title">Navigation</header>
         <Link to="/">Home</Link>
@@ -17,6 +23,7 @@ const Footer = () => {
         <header className="footer-title">About</header>
         <Link to="/about">About</Link>
       </nav>
+      <div id="wcb" className="carbonbadge"></div>
     </footer>
   );
 };
